f5ad8518021d6ad3abc369bf743218fd90499102
Kevin Hallett <kevinmhallett@gmail.com>
2019-01-19 08:14:27 -0600

    add game

---
+++ Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using RPSMusic;
+using RPSOutcome;
+using RPSInput;
+
+namespace RPSGame
+{
+    class Game
+    {
+
+        public void Start()
+        {
+            Music.Play(Music.Intro);
+            GameLoop();
+        }
+
+        public void GameLoop()
+        {
+            bool playAgain = true;
+            while (playAgain)
+            {
+                string playerInput = Input.GetPlayerInput();
+                string computerInput = Input.GetComputerInput();
+                GameOutcome.AnnounceGameOutcome(GameOutcome.CheckForWinner(playerInput, computerInput), playerInput, computerInput);
+
+                playAgain = Input.PlayAgain();
+                if (playAgain)
+                    Console.WriteLine("");
+            }
+        }
+    }
+}
